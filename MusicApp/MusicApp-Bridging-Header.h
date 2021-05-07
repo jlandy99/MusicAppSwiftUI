578f928b0823599fede0b99e0a0c15095d2df0b0
jlandy99 <jlandy99@github.com>
2021-05-07 16:43:03 -0400

    Imported Spotify framework, added bridging header and handled linking

---
+++ MusicApp/MusicApp-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  MusicApp-Bridging-Header.h
+//  MusicApp
+//
+//  Created by John Landy on 5/7/21.
+//  Copyright © 2021 John Landy. All rights reserved.
+//
+
+#ifndef MusicApp_Bridging_Header_h
+#define MusicApp_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* MusicApp_Bridging_Header_h */
